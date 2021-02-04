f6ea116f18067f36bd2a9c609eb1abbc018a972d
ccieescort <sethuvignesh@yahoo.co.in>
2021-02-04 07:02:08 -0500

    Day1

---
+++ PyBasics/device_connect.py
@@ -0,0 +1,22 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device1 = {
+     "host": "l3swa",
+     "username": "cisco",
+     "password": "123!Cisco",
+     "device_type": "cisco_ios",
+          }
+
+device2 = {
+     "host": "rtra",
+     "username": "cisco",
+     "password": "123!Cisco",
+     "device_type": "cisco_ios",
+          }
+
+for device in (device1,device2):
+    connect_device = ConnectHandler(**device)
+    print(connect_device.find_prompt())
+
+connect_device.disconnect()
