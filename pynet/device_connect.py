77af4042c9f435da6ca7313303c066e824d1e3cd
ccieescort <sethuvignesh@yahoo.co.in>
2021-03-14 10:14:55 +0100

    trying from NTT Laptop

--- pynet/device_connect.py
+++ pynet/device_connect.py
@@ -5,14 +5,14 @@ device1 = {
      "host": "rtra",
      "username": "cisco",
      "password": "123!Cisco",
-     "device_type": "cisco_ios",
+     "device_type": "cisco_ios"
           }
 
 device2 = {
      "host": "rtrb",
      "username": "cisco",
      "password": "123!Cisco",
-     "device_type": "cisco_ios",
+     "device_type": "cisco_ios"
           }
 
 for device in (device1,device2):
